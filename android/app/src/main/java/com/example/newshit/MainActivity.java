a99d902670591316cc72ac090d1c0ec0a6141559
unknown <druiz@sw1848.com>
2019-09-20 17:10:23 -0500

    prueba 1

--- android/app/src/main/java/com/example/newshit/MainActivity.java
+++ android/app/src/main/java/com/example/newshit/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//ljshdjlsjhjd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
